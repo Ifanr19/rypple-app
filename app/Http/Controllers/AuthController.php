da1343462e6f705a7536fa1aefeb84f2e10e1bc6
rzamzam <5026211052@mhs.its.ac.id>
2025-06-16 17:05:47 +0700

    Implement backend connection and Laravel login API

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user = User::where('username', $request->username)->first();
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            return response()->json(['success' => true]);
+        }
+
+        return response()->json(['success' => false], 401);
+    }
+}
