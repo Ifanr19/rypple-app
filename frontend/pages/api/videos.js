284b098282b1a1080164dd55c8af7d7c920cf90c
rayhanzamzam <120318647+rayhanzamzam@users.noreply.github.com>
2025-06-19 11:05:26 +0700

    Update videos.js

--- frontend/pages/api/videos.js
+++ frontend/pages/api/videos.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
     res.status(405).json({ message: 'Method not allowed' });
   }
 }
-=======
+
 import db from '../../lib/db';
 
 export default function handler(req, res) {
